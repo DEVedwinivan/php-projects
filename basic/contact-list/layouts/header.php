1c37e64417fc7ef4b19b6fb66258e08a7917f232
DEVedwinivan <156161349+DEVedwinivan@users.noreply.github.com>
2025-10-18 20:56:08 -0600

    Update header.php

--- basic/contact-list/layouts/header.php
+++ basic/contact-list/layouts/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Create Contact</title>
 </head>
 <body>
@@ -14,4 +14,5 @@
             <a href="index.php">Read</a>
             <a href="create-contact.php">Create</a>
         </nav>
+
     </header>
