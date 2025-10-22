618dcf34db23567ebf8da0b2e7ca3d0a83a4508d
DEVedwinivan <156161349+DEVedwinivan@users.noreply.github.com>
2025-10-22 17:38:37 -0600

    Update header.php

--- basic/contact-list/layouts/header.php
+++ basic/contact-list/layouts/header.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
+    <meta name="author" content="Edwin Velazquez">
+    <meta name="description" content="This page is a basic website where you can perform basic CRUD (create, read, update, and delete) operations. The data is stored without a database in a JSON file.">
     <link rel="stylesheet" href="css/style.css">
-    <title>Create Contact</title>
+    <title>Contact Manager</title>
 </head>
 <body>
     <header>
@@ -14,4 +16,5 @@
             <a href="index.php">Read</a>
             <a href="create-contact.php">Create</a>
         </nav>
+
     </header>
