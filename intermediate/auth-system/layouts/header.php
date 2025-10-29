4da7949014007f82cf006b82584b99b35573641d
DEVedwinivan <156161349+DEVedwinivan@users.noreply.github.com>
2025-10-28 19:29:42 -0600

    Add files via upload

---
+++ intermediate/auth-system/layouts/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <title>Auth System</title>
+</head>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary text-white"">
+        <div class="container-fluid">
+          <h1>Auth System</h1>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav d-flex justify-content-end align-items-end gap-3 mb-2 mb-lg-0 w-75">
+              <li class="nav-item"><a href="sign-in.php" class="btn btn-outline-light">Sign In</a></li>
+              <li class="nav-item"><a href="sign-up.php" class="btn btn-outline-light">Sign Up</a></li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+</header>
